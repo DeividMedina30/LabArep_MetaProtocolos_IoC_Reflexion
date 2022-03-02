2b67faf4acf851a889c06bc9374e31639753ae0b
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2022-03-01 22:11:15 -0500

    Creando clase Handler, para la reflexión.

---
+++ src/main/java/edu/escuelaing/arep/htttpServer/Handler.java
@@ -0,0 +1,28 @@
+package edu.escuelaing.arep.htttpServer;
+
+import java.lang.reflect.Method;
+
+public class Handler {
+    Method metodo;
+    public Handler(Method metodo){
+        this.metodo = metodo;
+    }
+
+    public String procesar() {
+        try {
+            return metodo.invoke(null, null).toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    public  String procesar(Object[] arg) {
+        try {
+            return metodo.invoke(metodo, arg).toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+}
