d1a4dd1b2eb009d44eef2c257721a2f492f8c5aa
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2022-03-02 08:53:26 -0500

    Creando App calculadora

--- src/main/java/app/calculadora.java
+++ src/main/java/app/calculadora.java
@@ -6,6 +6,9 @@ public class calculadora {
 
     @Web("resultadoSuma")
     public static String resultadoSumallamar(String valorUno, String valorDos){
+        int numeroUno = Integer.parseInt (valorUno);
+        int numeroDos = Integer.parseInt (valorDos);
+        String resultadoSumatoria = String.valueOf(numeroUno+numeroDos);
         return"<!DOCTYPE html>\n"
                 +"<html>\n"
                 +"<head>\n"
@@ -13,7 +16,8 @@ public class calculadora {
                 +"  <title>Proyecto AREM</title>  \n"
                 +"</head>\n"
                 +"<body style=\"text-align: center\">   \n"
-                + "<h1>Resultado de la Suma de: "+valorUno+" + "+valorDos+" es igual a:   </h1>"
+                + "<h1>Resultado de la Suma de: "+valorUno+" + "+valorDos+" es igual a: "+resultadoSumatoria+"  </h1>"
+                + "<h1>Resultado de la Suma de: "+valorUno+" + "+valorDos+" es igual a: "+resultadoSumatoria+"  </h1>"
                 +"</body>\n"
                 +"</html>\n"
                 +"";
@@ -32,7 +36,9 @@ public class calculadora {
                 +"<form action=\"/app/resultadoSuma\">	\n"
                 + "  <input id='calculadoraIdnumeroUno' type='number' name='primernumero' class='form-control' placeholder='Ingrese el primer número a sumar' >"
                 + "<br>"
+                + "<br>"
                 + "  <input id='calculadoraIdnumeroDos' type='number' name='segundonumero' class='form-control' placeholder='Ingrese el segundo número a sumar' >"
+                + "<br>"
                 + "<br>"
                 + "  <input id='calculadora_button_id' type='submit' value='Realizar la suma' class='btns'>"
                 +"</form>"
