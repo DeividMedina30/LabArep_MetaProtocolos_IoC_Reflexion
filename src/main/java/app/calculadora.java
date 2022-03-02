bec8ee2f90249e9002af862ac53cffb726d3f194
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2022-03-02 08:57:42 -0500

    Creando App calculadora

--- src/main/java/app/calculadora.java
+++ src/main/java/app/calculadora.java
@@ -17,14 +17,31 @@ public class calculadora {
                 +"</head>\n"
                 +"<body style=\"text-align: center\">   \n"
                 + "<h1>Resultado de la Suma de: "+valorUno+" + "+valorDos+" es igual a: "+resultadoSumatoria+"  </h1>"
-                + "<h1>Resultado de la Suma de: "+valorUno+" + "+valorDos+" es igual a: "+resultadoSumatoria+"  </h1>"
                 +"</body>\n"
                 +"</html>\n"
                 +"";
     }
 
-    @Web("calculadora")
-    public static String ejemploCalculadora(){
+    @Web("resultadoResta")
+    public static String resultadoRestallamar(String valorUno, String valorDos){
+        int numeroUno = Integer.parseInt (valorUno);
+        int numeroDos = Integer.parseInt (valorDos);
+        String resultadoSubstracion = String.valueOf(numeroUno+numeroDos);
+        return"<!DOCTYPE html>\n"
+                +"<html>\n"
+                +"<head>\n"
+                +"  <meta charset=\"utf-8\" />\n"
+                +"  <title>Proyecto AREM</title>  \n"
+                +"</head>\n"
+                +"<body style=\"text-align: center\">   \n"
+                + "<h1>Resultado de la Suma de: "+valorUno+" + "+valorDos+" es igual a: "+resultadoSubstracion+"  </h1>"
+                +"</body>\n"
+                +"</html>\n"
+                +"";
+    }
+
+    @Web("calculadoraSumar")
+    public static String ejemploCalculadoraSuma(){
         return"<!DOCTYPE html>\n"
                 +"<html>\n"
                 +"<head>\n"
@@ -38,6 +55,30 @@ public class calculadora {
                 + "<br>"
                 + "<br>"
                 + "  <input id='calculadoraIdnumeroDos' type='number' name='segundonumero' class='form-control' placeholder='Ingrese el segundo número a sumar' >"
+                + "<br>"
+                + "<br>"
+                + "  <input id='calculadora_button_id' type='submit' value='Realizar la suma' class='btns'>"
+                +"</form>"
+                +"</body>\n"
+                +"</html>\n"
+                +"";
+    }
+
+    @Web("calculadoraRestar")
+    public static String ejemploCalculadoraRestar(){
+        return"<!DOCTYPE html>\n"
+                +"<html>\n"
+                +"<head>\n"
+                +"  <meta charset=\"utf-8\" />\n"
+                +"  <title>Proyecto AREM</title>  \n"
+                +"</head>\n"
+                +"<body style=\"text-align: center\">   \n"
+                + "<h1>Calculadora que resta dos números </h1>"
+                +"<form action=\"/app/resultadoResta\">	\n"
+                + "  <input id='calculadoraIdnumeroUno' type='number' name='primernumero' class='form-control' placeholder='Ingrese el primer número a restar.' >"
+                + "<br>"
+                + "<br>"
+                + "  <input id='calculadoraIdnumeroDos' type='number' name='segundonumero' class='form-control' placeholder='Ingrese el segundo número a restar.' >"
                 + "<br>"
                 + "<br>"
                 + "  <input id='calculadora_button_id' type='submit' value='Realizar la suma' class='btns'>"
