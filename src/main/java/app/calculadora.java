8882c0badb5327ad3b7b9c3c96b223193b46a527
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2022-03-02 08:43:26 -0500

    Creando App calculadora

--- src/main/java/app/calculadora.java
+++ src/main/java/app/calculadora.java
@@ -4,6 +4,21 @@ import edu.escuelaing.arep.htttpServer.Web;
 @Web("Services")
 public class calculadora {
 
+    @Web("resultadoSuma")
+    public static String resultadoSumallamar(String valorUno){
+        return"<!DOCTYPE html>\n"
+                +"<html>\n"
+                +"<head>\n"
+                +"  <meta charset=\"utf-8\" />\n"
+                +"  <title>Proyecto AREM</title>  \n"
+                +"</head>\n"
+                +"<body style=\"text-align: center\">   \n"
+                + "<h1>Resultado de la Suma de: "+valorUno+" + segundo número es igual a:   </h1>"
+                +"</body>\n"
+                +"</html>\n"
+                +"";
+    }
+
     @Web("calculadora")
     public static String ejemploCalculadora(){
         return"<!DOCTYPE html>\n"
@@ -14,9 +29,9 @@ public class calculadora {
                 +"</head>\n"
                 +"<body style=\"text-align: center\">   \n"
                 + "<h1>Calculadora que suma dos números </h1>"
-                +"<form action=\"/app/ejemplo\">	\n"
+                +"<form action=\"/app/resultadoSuma\">	\n"
                 + "  <input id='calculadoraId' type='number' name='primernumero' class='form-control' placeholder='Ingrese el número a sumar' >"
-                + "  <input id='calculadora_button_id' type='submit' value='Realizar la suma' class='btns' action=\"/app/ejemplo\">"
+                + "  <input id='calculadora_button_id' type='submit' value='Realizar la suma' class='btns'>"
                 +"</form>"
                 +"</body>\n"
                 +"</html>\n"
